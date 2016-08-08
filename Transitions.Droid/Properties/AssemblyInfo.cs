c1346aeeb779b830e92b7db90fc24a3361b157cc
Adam Hewitt <adamhewitt@olivetree.com>
2016-08-08 07:15:53 -0700

    Add assembly info

---
+++ Transitions.Droid/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Resources;
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OliveTree Transitions - Android")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Olive Tree Bible Software")]
+[assembly: AssemblyProduct("OliveTree.Transitions")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
